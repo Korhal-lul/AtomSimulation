98d59025eee1c9ec10888653b9a444422c21bf3d
Korhal <ninj4Noob@hotmail.com>
2020-02-15 11:22:20 -0300

    Inertia! maybe

--- src/main/java/br/com/senai/model/Proton.java
+++ src/main/java/br/com/senai/model/Proton.java
@@ -19,6 +19,7 @@ public class Proton extends Particle {
     private PApplet view;
     private int numBalls;
     private boolean hold = false;
+    private boolean released = false;
 
     public Proton(float xin, float yin, float din, int id, Particle[] particles, PApplet view, float spring, float gravity, float friction) {
         this.x = xin;
@@ -65,6 +66,101 @@ public class Proton extends Particle {
     }
 
     @Override
+    public void inertia() {
+
+        if (hold) return;
+        PVector dir = PVector.sub(new PVector(view.mouseX, view.mouseY), new PVector(view.pmouseX, view.pmouseY));
+        float magnitude = dir.mag();
+
+        float angle = degrees(dir.heading());
+        int pmouseX =  view.pmouseX;
+        int pmouseY =  view.pmouseY;
+
+        if(pmouseX < x){
+            pmouseX*=-1;
+        }
+        if(pmouseY < y){
+            pmouseY*=-1;
+        }
+        float targetX = pmouseX + cos(angle) * magnitude;
+        float targetY = pmouseY + sin(angle) * magnitude;
+        float ax = (targetX - x) * spring;
+        float ay = (targetY - y) * spring;
+        String info = "Degrees: " + (int) degrees(dir.heading()) + "\nMagnitude: " + (int) magnitude;
+        System.out.println(info);
+        System.out.println("ax = " + ax + " ay = " + ay + " Spring " + spring);
+        System.out.println("pmouseX = " + view.pmouseX + " pmouseY = " + view.pmouseY);
+        vx += ax;
+        vy += ay;
+
+        /*Work Later
+        if (hold) return;
+
+        PVector dir = PVector.sub(new PVector(view.mouseX, view.mouseY), new PVector(view.pmouseX, view.pmouseY));
+        float magnitude = dir.mag();
+        String info = "Degrees: " + (int) degrees(dir.heading()) + "\nMagnitude: " + (int) magnitude;
+        System.out.println(info);
+        */
+
+    }
+
+    public void collide() {
+        for (int i = 0; i < numBalls; i++) {
+            if (id == i) break;
+            float dx = others[i].getX() - x;
+            float dy = others[i].getY() - y;
+            float distance = sqrt(dx * dx + dy * dy);
+            float minDist = others[i].getDiameter() / 2 + diameter / 2;
+            if (distance < minDist) {
+                float angle = atan2(dy, dx);
+                float targetX = x + cos(angle) * minDist;
+                float targetY = y + sin(angle) * minDist;
+                float ax = (targetX - others[i].getX()) * spring;
+                float ay = (targetY - others[i].getY()) * spring;
+                vx -= ax;
+                vy -= ay;
+                others[i].setVX(others[i].getVX() + ax);
+                others[i].setVY(others[i].getVY() + ay);
+            }
+        }
+    }
+
+    public void moveGravity() {
+        /*Unused for now
+        vy += gravity;
+        x += vx;
+        y += vy;
+
+        }*/
+    }
+
+    public void move() {
+        float radius = diameter / 2;
+
+        if (x + radius > view.width) {
+            x = view.width - radius;
+            vx *= friction;
+        } else if (x - radius < 0) {
+            x = radius;
+            vx *= friction;
+        }
+        if (y + radius > view.height) {
+            y = view.height - radius;
+            vy *= friction;
+        } else if (y - radius < 0) {
+            y = radius;
+            vy *= friction;
+        }
+    }
+
+    public void display() {
+        x += vx;
+        y += vy;
+        view.ellipse(this.x, this.y, diameter, diameter);
+    }
+
+
+    @Override
     public float getX() {
         return x;
     }
@@ -109,67 +205,4 @@ public class Proton extends Particle {
         this.vy = vy;
     }
 
-    @Override
-    public void inertia() {
-        if (hold) return;
-
-        PVector dir = PVector.sub(new PVector(view.mouseX, view.mouseY), new PVector(view.pmouseX, view.pmouseY));
-        float magnitude = dir.mag();
-        String info = "Degrees: " + (int) degrees(dir.heading()) + "\nMagnitude: " + (int) magnitude;
-        System.out.println(info);
-
-    }
-
-    public void collide() {
-        for (int i = 0; i < numBalls; i++) {
-            if (id == i) break;
-            float dx = others[i].getX() - x;
-            float dy = others[i].getY() - y;
-            float distance = sqrt(dx * dx + dy * dy);
-            float minDist = others[i].getDiameter() / 2 + diameter / 2;
-            if (distance < minDist) {
-                float angle = atan2(dy, dx);
-                float targetX = x + cos(angle) * minDist;
-                float targetY = y + sin(angle) * minDist;
-                float ax = (targetX - others[i].getX()) * spring;
-                float ay = (targetY - others[i].getY()) * spring;
-                vx -= ax;
-                vy -= ay;
-                others[i].setVX(others[i].getVX() + ax);
-                others[i].setVY(others[i].getVY() + ay);
-            }
-        }
-    }
-
-    public void moveGravity() {
-        /*Unused for now
-        vy += gravity;
-        x += vx;
-        y += vy;
-
-        }*/
-    }
-
-    public void move() {
-        if (x + diameter / 2 > view.width) {
-            x = view.width - diameter / 2;
-            vx *= friction;
-        } else if (x - diameter / 2 < 0) {
-            x = diameter / 2;
-            vx *= friction;
-        }
-        if (y + diameter / 2 > view.height) {
-            y = view.height - diameter / 2;
-            vy *= friction;
-        } else if (y - diameter / 2 < 0) {
-            y = diameter / 2;
-            vy *= friction;
-        }
-    }
-
-    public void display() {
-        x += vx;
-        y += vy;
-        view.ellipse(this.x, this.y, diameter, diameter);
-    }
 }
